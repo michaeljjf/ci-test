e66f82453d0bbe9e4d5a1496374a2a6a7a61296c
蒋建飞 <jiangjf@mingyuanyun.com>
2023-01-22 21:45:24 +0800

    test

--- src/test/java/com/jiangjf/citest/CiTestApplicationTests.java
+++ src/test/java/com/jiangjf/citest/CiTestApplicationTests.java
@@ -20,7 +20,7 @@ class CiTestApplicationTests {
     public void testAdd()
     {
         int sum = calcService.add(1, 3);
-        assert sum == 3;
+        assert sum == 4;
         System.out.println("test add 呵呵！！");
     }
 
